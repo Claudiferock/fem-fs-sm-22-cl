e588d96acf9c9d2d15c98e3e4d18b8b490e8bae3
Claudiferock <claudio.rodriguez.vasquez@myy.haaga-helia.fi>
2022-04-13 10:27:48 +0300

    Add route handlers protection

---
+++ lib/auth.ts
@@ -0,0 +1,37 @@
+// helpers for authorization
+import jwt from 'jsonwebtoken';
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from './prisma';
+
+// higher order func as middleware to protect API routes (gatekeeper)
+export const validateRoute = (handler) => {
+  // it will check if you have a token in your cookie
+  // if it's a valid user then you can call the handler
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    const token = req.cookies.TRAX_ACCESS_TOKEN
+
+    if (token) {
+      let user
+
+      try {
+        // check that it's still a valid user (in case of user being deleted from db)
+        const { id } = jwt.verify(token, "hello");
+        user = await prisma.user.findUnique({
+          where: { id },
+        });
+        if (!user) {
+          throw new Error("Not a real user");
+        }
+      } catch (error) {
+        res.status(401);
+        res.json({ error: "Not Authorized" });
+        return;
+      }
+      // passing it along to the next handler
+      return handler(req, res, user)
+    }
+
+    res.status(401);
+    res.json({ error: "Not Authorized" });
+  }
+}
