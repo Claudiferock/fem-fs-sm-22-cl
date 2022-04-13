f17087361fb5ede38686330602b58fba13782e85
Claudiferock <claudio.rodriguez.vasquez@myy.haaga-helia.fi>
2022-04-13 14:21:12 +0300

    Add main section header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
 import Head from "next/head";
 import Image from "next/image";
+import GradientLayout from "../components/gradientLayout";
 import styles from "../styles/Home.module.css";
 
 export const Home = () => {
-  return <>Home</>
+  return (
+    <GradientLayout
+      roundImage
+      color="red"
+      subtitle="profile"
+      title="Person Perssonen"
+      description="x public playlists"
+    >
+      <h2>Home Page</h2>
+    </GradientLayout>
+  );
 };
 
 export default Home;
