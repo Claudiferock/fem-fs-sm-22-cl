b89c514af985bab8ae13fe0061c3b5f2b6ce7c39
Claudiferock <claudio.rodriguez.vasquez@myy.haaga-helia.fi>
2022-04-20 10:14:01 +0300

    Fix typo

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -43,7 +43,7 @@ export const getServerSideProps = async ({ query, req }) => {
     return {
       redirect: {
         permanent: false,
-        destination: "/";
+        destination: "/",
       }
     }
   }
